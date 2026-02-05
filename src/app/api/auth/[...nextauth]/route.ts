bf757ab4b430797a4ac8587e007d145ecd10392e
Ajay Sidal <ajay.sidal@opsvantagedigital.online>
2026-02-05 14:11:26 +1300

    fix: export authOptions to fix build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth, { type NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -19,6 +19,8 @@ const handler = NextAuth({
     })
   ],
   session: { strategy: "jwt" }
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
